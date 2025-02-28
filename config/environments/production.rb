a526c0f34b43f6d47fc6f722a16e3bfb3edc7026
Helder Santos <hpsantos90@gmail.com>
2025-02-28 14:19:09 +0000

    allow https origins for actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   #
 
-  config.action_cable.allowed_request_origins = [ "http://159.69.82.98" ]
+  config.action_cable.allowed_request_origins = [ "http://159.69.82.98", "https://159.69.82.98" ]
 end
