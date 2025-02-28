a9da1cc6e3ceabaa05f036049c54d262ba963a78
Helder Santos <hpsantos90@gmail.com>
2025-02-28 14:22:37 +0000

    skip auth token on create

--- app/controllers/rover/messages_controller.rb
+++ app/controllers/rover/messages_controller.rb
@@ -3,6 +3,8 @@ module Rover
     # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
     allow_browser versions: :modern
 
+    skip_before_action :verify_authenticity_token, only: [ :create ]
+
     def create
       Message.create(params[:message].permit(:player_id, :game_id, :body))
 
